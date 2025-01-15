40f2b3465d0ca82eeeb4cced0aa4fa05d2e5b56e
Armagan Amcalar <armagan@amcalar.com>
2025-01-15 22:58:37 +0100

    RTC 4

--- src/App.vue
+++ src/App.vue
@@ -54,29 +54,20 @@ async function getBestVideoDevice() {
 
 const config = {
   iceServers: [
-    { urls: 'stun:stun.l.google.com:19302' },
-    { urls: 'stun:stun1.l.google.com:19302' },
-    { urls: 'stun:stun2.l.google.com:19302' },
     {
-      urls: 'turn:openrelay.metered.ca:80',
-      username: 'openrelayproject',
-      credential: 'openrelayproject'
+      urls: ['stun:stun.l.google.com:19302', 'stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302']
     },
     {
-      urls: 'turn:openrelay.metered.ca:443',
-      username: 'openrelayproject',
-      credential: 'openrelayproject'
-    },
-    {
-      urls: 'turn:openrelay.metered.ca:443?transport=tcp',
+      urls: ['turns:openrelay.metered.ca:443', 'turn:openrelay.metered.ca:443', 'turn:openrelay.metered.ca:80'],
       username: 'openrelayproject',
       credential: 'openrelayproject'
     }
   ],
   iceCandidatePoolSize: 10,
-  iceTransportPolicy: 'relay',
   bundlePolicy: 'max-bundle',
-  rtcpMuxPolicy: 'require'
+  rtcpMuxPolicy: 'require',
+  sdpSemantics: 'unified-plan',
+  iceTransportPolicy: 'all'
 }
 
 async function initializeWebRTC() {
